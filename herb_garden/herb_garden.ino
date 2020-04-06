1f92a60e2852a8766a5f31b9d2954d89c590ba0e
Jasper Boogers <jasperboogers@gmail.com>
2020-04-06 22:19:51 +0200

    added new definitions

--- herb_garden/herb_garden.ino
+++ herb_garden/herb_garden.ino
@@ -12,6 +12,29 @@
 #define MotorBoundPinClose 9 // End stop module next to controller
 #define MotorBoundPinFar 10 // End stop module at far end of rail
 
+// define motor directions and locations
+#define FORWARD LOW
+#define BACKWARD HIGH
+float CurrentLocation;
+float MinLocation;
+float MaxLocation;
+
+/* define plant locations
+ *  3 is furthest from watering head base station, 1 is nearest
+ *  right is defined looking from base station along the guidance rail
+ *  should locations be in percentages?
+ */
+#define PlantLocation1 3
+#define PlantDirection1 "right"
+#define PlantLocation2 2
+#define PlantDirection2 "right"
+#define PlantLocation3 1
+#define PlantDirection3 "right"
+#define PlantLocation4 3
+#define PlantDirection4 "left"
+#define PlantLocation5 2
+#define PlantDirection5 "left"
+
 void setup() {
   // put your setup code here, to run once:
   pinMode(MoisturePin1, INPUT);
@@ -35,9 +58,11 @@ void loop() {
 
 void setup_motor() {
   // function to find bounds of rails
+  // set MinLocation and MaxLocation
   
 }
 
-void move_motor(dir, steps){
-  // 
+void move_motor(location){
+  // move motor to location
+  // use CurrentLocation to determine direction of motion
 }
