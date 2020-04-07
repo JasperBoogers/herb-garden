dc4c99ea4c13059eaab16700f178418735618c2e
Jasper Boogers <jasperboogers@gmail.com>
2020-04-07 21:23:34 +0200

    refactor

--- herb_garden/herb_garden.ino
+++ herb_garden/herb_garden.ino
@@ -8,8 +8,8 @@
 #define MoisturePin4 4
 #define MoisturePin5 5
 #define LDRPin 6
-#define DirPin 7
-#define StepPin 8
+#define MotorDirPin 7
+#define MotorStepPin 8
 #define MotorBoundPinClose 9 // End stop module next to controller
 #define MotorBoundPinFar 10 // End stop module at far end of rail
 
@@ -21,35 +21,19 @@ float MinLocation;
 float MaxLocation;
 
 // define plants
-struct Plant1 {
-  int location = 3;
-  char dir[6] = "right";
+struct Plant {
+  public:
+  int location;
+  char dir[6];
   char species[15];
+  float sensor_threshold;
   };
 
-struct Plant2 {
-  int location = 2;
-  char dir[6] = "right";
-  char species[15];
-  };
-
-struct Plant3 {
-  int location = 1;
-  char dir[6] = "right";
-  char species[15];
-  };
-
-struct Plant4 {
-  int location = 3;
-  char dir[6] = "left";
-  char species[15];
-  };
-
-struct Plant5 {
-  int location = 2;
-  char dir[6] = "left";
-  char species[15];
-  };
+Plant plant1 = {3, "right", "basilicum", 100.0};
+Plant plant2 = {3, "left", "tijm", 105.0};
+Plant plant3 = {2, "right", "munt", 88.4};
+Plant plant4 = {2, "left", "koriander", 150};
+Plant plant5 = {1, "right", "oregano", 99.0};
 
 void setup() {
   // put your setup code here, to run once:
@@ -59,29 +43,43 @@ void setup() {
   pinMode(MoisturePin4, INPUT);
   pinMode(MoisturePin5, INPUT);
   pinMode(LDRPin, INPUT);
-  pinMode(DirPin, OUTPUT);
-  pinMode(StepPin, OUTPUT);
+  pinMode(MotorDirPin, OUTPUT);
+  pinMode(MotorStepPin, OUTPUT);
   pinMode(MotorBoundPinClose, INPUT);
   pinMode(MotorBoundPinFar, INPUT);
-  MinLocation = setup_motor(MaxLocation);
+  MinLocation = find_min_location();
+  MaxLocation = find_max_location();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  /*
+   * loop over plants
+   * for each plant, read sensor value and compare to threshold
+   * if a plant is dry, move to that plant and water it
+   * once watered, do not move watering head, but continue checking
+   * if all plants watered, move back to MinLocation 
+   */
 }
 
 
-float setup_motor(float Max) {
-  // function to find bounds of rails
-  // set MinLocation and MaxLocation
-
-//  Max = ;
-//  Min = ;
+float find_min_location(){
+//  float Min:
 //  return Min;
+}
+
+float find_max_location(){
+//  float Max;
+//  return Max;
 }
 
 void move_motor(float location){
   // move motor to location
   // use CurrentLocation to determine direction of motion
+}
+
+float check_water_level(){
+  // use ultrasonic sensor to determine water reservoir height
+  float height;
+  return height;
 }
